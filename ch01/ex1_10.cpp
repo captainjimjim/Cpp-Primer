c0151c83f9df2b6e73a0b84b220a12c570e6a2a2
captainjimjim <do.jimmy@courrier.uqam.ca>
2017-07-04 13:56:09 -0400

    Update ex1_10.cpp
    Following the book's example

--- ch01/ex1_10.cpp
+++ ch01/ex1_10.cpp
@@ -1,11 +1,21 @@
-// prints the numbers from ten down to zero.(use while)
-
 #include <iostream>
-
 int main()
+
 {
-    int i = 10;
-    while (i >= 0)
-        std::cout << i-- << " ";
+    int sum = 0, val = 10;
+    
+    while (val >= 0)
+    {
+        sum += val;
+        val--;
+    }
+    
+
+    std::cout << "The sum of the numbers ranging from 0 to 10 inclusively is " << sum << std::endl;
     return 0;
+
+
+
+
+
 }
